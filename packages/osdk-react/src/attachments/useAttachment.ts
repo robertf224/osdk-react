65ef36467a2b9c836c205f60e4c77f08b4b4b298
Robert Fidler <robertf224@gmail.com>
2025-06-25 17:16:23 -0400

    move to data urls

--- packages/osdk-react/src/attachments/useAttachment.ts
+++ packages/osdk-react/src/attachments/useAttachment.ts
@@ -1,13 +1,14 @@
 import { Attachment } from "@osdk/api";
 import { useSuspenseQuery, UseSuspenseQueryResult } from "@tanstack/react-query";
+import { blobToDataUrl } from "../utils";
 
 export function useAttachment(attachment: Attachment): UseSuspenseQueryResult<string> {
     return useSuspenseQuery({
-        queryKey: ["osdk", "attachment-url", attachment.rid],
+        queryKey: ["osdk", "attachment", attachment.rid],
         queryFn: async () => {
             const contents = await attachment.fetchContents();
             const blob = await contents.blob();
-            return URL.createObjectURL(blob);
+            return blobToDataUrl(blob);
         },
         staleTime: Infinity,
     });
--- packages/osdk-react/src/users/useUserProfilePicture.ts
+++ packages/osdk-react/src/users/useUserProfilePicture.ts
@@ -4,7 +4,7 @@ import { Users } from "@osdk/foundry.admin";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { UseSuspenseQueryResult } from "@tanstack/react-query";
 import { useOsdkContext } from "../OsdkContext";
-
+import { blobToDataUrl } from "../utils/blobToDataUrl";
 export function useUserProfilePicture(userId: string): UseSuspenseQueryResult<string | null> {
     const { client } = useOsdkContext();
     return useSuspenseQuery({
@@ -13,7 +13,7 @@ export function useUserProfilePicture(userId: string): UseSuspenseQueryResult<st
             // Typing is wrong here.
             if (result) {
                 const blob = await result.blob();
-                return URL.createObjectURL(blob);
+                return blobToDataUrl(blob);
             }
             return null;
         },
